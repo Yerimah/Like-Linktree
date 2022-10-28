60870c815c13d4749c0523cb5b5a772fd7f40e49
Yerimah <danielyerimah@yahoo.com>
2022-10-28 18:12:16 +0100

    create anchor tags

--- src/App.js
+++ src/App.js
@@ -11,6 +11,32 @@ const App = () => {
       <Avatar id="profile_img" image="profilepic.png" size="xlarge">
         <Badge value="4" severity="danger" />
       </Avatar>
+      <div id="twitter">iamdeewyne</div>
+      <div id="slack"></div>
+
+      <div className="links">
+       <button>
+        <a id="btn__zuri" href="https://training.zuri.team/">
+          Zuri team
+        </a>
+       </button>
+
+        <button>
+          <a id="books" href="http://books.zuri.team">Designing and Coding Books</a>
+        </button>
+
+        <button>
+          <a id="book__python" href="https://books.zuri.team/">Python Books</a>
+        </button>
+
+        <button>
+          <a id="pitch" href="https://background.zuri.team"></a>
+        </button>
+
+        <button>
+          <a id="book__design" href="https://books.zuri.team/design-rules">Design Books</a>  
+        </button>
+      </div>
     </div>
   );
 }
