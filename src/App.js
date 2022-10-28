0b34eccf694d41d74bf3c98c7e08f83bb928830b
Yerimah <danielyerimah@yahoo.com>
2022-10-28 12:52:33 +0100

    create profile picture and install primereact

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";
+import { Avatar } from 'primereact/avatar';
+import { Badge } from 'primereact/badge';
+import "./App.css"
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      
+      <Avatar id="profile_img" image="profilepic.png" size="xlarge">
+        <Badge value="4" severity="danger" />
+      </Avatar>
     </div>
   );
 }
