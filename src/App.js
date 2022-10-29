6cc2daed9bff970d1b18c7fcd34712ad7f491336
Yerimah <danielyerimah@yahoo.com>
2022-10-29 02:50:04 +0100

    Remove unused codes from my codebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-// import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
-// import "primereact/resources/primereact.min.css";                  //core css
-// import "primeicons/primeicons.css";
-// import { Avatar } from 'primereact/avatar';
-// import { Badge } from 'primereact/badge';
 import Profile from './components/Profile';
 import Links from './components/Links';
 import Footer from './components/Footer';
