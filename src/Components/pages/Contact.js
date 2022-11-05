1139ace61354296aabb20d045a0af4980665dcd3
Yerimah <danielyerimah@yahoo.com>
2022-11-05 22:43:22 +0100

    Add form input validation

--- src/Components/pages/Contact.js
+++ src/Components/pages/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
     message: '',
   });
 
-  const [submit, setSubmit] = useState(false)
+  const [submit, setSubmit] = useState(false);
 
   const handleFirstInput = (event) => {
     setInput({ ...input, firstname: event.target.value });
@@ -28,13 +28,13 @@ const Contact = () => {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    setSubmit(true)
+    e.preventDefault();
+    setSubmit(true);
   };
 
   return (
     <>
-      <div className='contact-form'>
+      <div className="contact-form">
         <div>
           <h1 className="contact_head">Contact Me</h1>
           <p className="ama">Hi there, contact me to ask about anything you have in mind</p>
@@ -43,61 +43,63 @@ const Contact = () => {
         <form onSubmit={handleSubmit}>
           <div className="first_last">
             <div className="block">
-            <label>First name</label>
+              <label htmlFor="name">First name</label>
               <input
                 onChange={handleFirstInput}
                 value={input.firstname}
                 id="first_name"
                 className="styl_name"
                 type="text"
-                name="firstName"
+                name="name"
                 placeholder="Enter your first name"
               />
-            </div>
               {submit && !input.firstname ? <span>Please enter a first name</span> : null }
-            
+            </div>
+
             <div className="block">
-            <label>Last name</label>
+              <label htmlFor="lastname">Last name</label>
               <input
                 onChange={handleLastInput}
                 value={input.lastname}
                 id="last_name"
                 className="styl_name"
                 type="text"
-                name="lastName"
+                name="lastname"
                 placeholder="Enter your last name"
               />
-             </div> 
-            {submit && !input.lastname ? <span>Please enter a last name</span> : null}
+              {submit && !input.lastname ? <span>Please enter a last name</span> : null}
+            </div>
+
           </div>
 
-          <label>Email</label>
-            <input
-              onChange={handleEmail}
-              value={input.email}
-              id="email"
-              type="email"
-              placeholder="yourname@email.com"
-            />
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={handleEmail}
+            value={input.email}
+            id="email"
+            type="email"
+            placeholder="yourname@email.com"
+          />
           {submit && !input.email ? <span>Please enter your email</span> : null }
-          
-          <label>Message</label>
-            <textarea
-              onChange={handleMessage}
-              value={input.message}
-              id="message"
-              name="message"
-              placeholder="Send me a message and I will reply you as soon as posible"
-              maxLength="50"
-            />
+
+          <label htmlFor="message">Message</label>
+          <textarea
+            onChange={handleMessage}
+            value={input.message}
+            id="message"
+            name="message"
+            placeholder="Send me a message and I will reply you as soon as posible"
+            maxLength="50"
+          />
           {submit && !input.message ? <span>Please enter a message</span> : null }
-          
-          <div>
-            <p>
+
+          <div className="check">
+            <input type="checkbox" name="check" />
+            <label htmlFor="check">
               You agree to providing your data to
               {' '}
               who may contact you
-            </p>
+            </label>
           </div>
 
           <input type="submit" value="Send message" id="btn_submit" />
